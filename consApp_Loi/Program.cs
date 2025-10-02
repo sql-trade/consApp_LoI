934ca4d75139881a050cf9a135b851a211e6012c
sql-trade <sql-trade_github@sql-fabrik.de>
2025-10-02 10:22:45 +0200

    step 01: GetLatestFileName()

---
+++ consApp_Loi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace MyApp
+{
+    public class MyClass
+    {
+        static void Main()
+        {
+            string path = @"C:\Users\" + Environment.UserName + @"\AppData\Roaming\ATAS\Indicators\cache\tradeneon";
+            string filter = @"*ES*";
+
+            Console.WriteLine("begin ...");
+            Console.WriteLine("");
+
+            var mc = new MyClass();
+            string result = mc.GetLatestFileName(path, filter);
+
+            Console.WriteLine("Filename: " + result);
+            Console.WriteLine("");
+            Console.WriteLine("habe fertig.");
+        }
+
+        public string GetLatestFileName(string DirectoryName, string FileMask)
+        {
+            DateTime newestDate = DateTime.MinValue;
+            int newestIndex = 0;
+            FileInfo[] Files = (new DirectoryInfo(DirectoryName)).GetFiles(FileMask);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                if (newestDate < Files[i].LastWriteTime)
+                {
+                    newestDate = Files[i].LastWriteTime;
+                    newestIndex = i;
+                }
+            }
+            return (Files.Length > 0) ? Files[newestIndex].FullName : null;
+        }
+    }
+}
